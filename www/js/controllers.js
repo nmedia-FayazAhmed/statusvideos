601ff7bc707d9093706b8bbc411f424af9534bf6
FayazAhmed <fayaz.nmedia@gmail.com>
2017-09-13 13:34:07 +0500

    style final

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,69 +1,17 @@
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout, $ionicPopup) {
-  $scope.playmyvedio = function(){
-    VideoPlayer.play("file:///android_asset/www/movie.mp4");
-  }
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-    alert('heloo word');
-    
-  };
-  $scope.showAlert = function() {
-       var alertPopup = $ionicPopup.alert({
-         title: 'Don\'t eat that!',
-         template: 'It might taste good'
-       });
-
-       alertPopup.then(function(res) {
-         console.log('Thank you for not eating my delicious ice cream cone');
-       });
-     };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+  
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+.controller('searchCtrl', function($scope) {
+  document.getElementById('myframe').onload = function() {
+      // alert('myframe is loaded');
+      document.getElementById("red").style.display = 'none';
+      document.getElementsByClassName("ytp-youtube-button")[0].style.display = 'none';
+    };
+      // alert('myframe is loaded');
+
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
